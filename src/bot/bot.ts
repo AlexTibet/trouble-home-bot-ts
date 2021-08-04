6595499981420bfadb5c3123dc977a0b8414d230
Alexandr_Sobko <alexfreetibet@gmail.com>
2021-08-04 18:20:50 +0700

    create simle handler

--- src/bot/bot.ts
+++ src/bot/bot.ts
@@ -1,7 +1,34 @@
 import { Client, Message } from "discord.js";
 
+interface IMessageHandler {
+  readonly type: string;
+  readonly message: Message;
+}
+
+abstract class MessageHandler implements IMessageHandler {
+  abstract readonly type: string;
+  abstract readonly message: Message;
+  abstract getAnswer(Client, any): void
+}
+
+class PingHandler extends MessageHandler {
+  type: string;
+  message: Message;
+
+  constructor(handlerType: string, message: Message) {
+    super();
+    this.type = handlerType;
+    this.message = message
+  }
+  getAnswer(client: Client, channel: any) {
+    const data: string = 'pong'
+    console.log(data)
+    this.message.reply(data)
+  }
+}
+
 export class Bot {
-  private client: Client;
+  private readonly client: Client;
   private readonly token: string;
 
   constructor() {
@@ -12,7 +39,18 @@ export class Bot {
   public listen(): Promise<string> {
 
     this.client.on('message', (message: Message) => {
+      let handler: MessageHandler;
+      switch (message.content) {
+        case 'ping':
+          handler = new PingHandler('ping', message);
+          handler.getAnswer(this.client, message.channel)
+          break
+        case 'pong':
+          handler = new PingHandler('pong', message);
+          handler.getAnswer(this.client, message.channel)
+      }
       console.log("Message received! Contents: ", message.content);
+      console.log(handler)
     });
 
     this.client.on('ready', () => {
