8f0f2f92494e996c51f53924708e868409e05c91
Alexandr_Sobko <alexfreetibet@gmail.com>
2021-12-07 16:24:50 +0700

    fix ready

--- src/bot/events/ready.ts
+++ src/bot/events/ready.ts
@@ -1,20 +1,18 @@
 import { Client } from 'discord.js';
 import { IEvent } from '../interfaces';
+import options from '../../config/options';
 
 const event: IEvent = {
   name: 'ready',
   once: true,
   async execute(client: Client): Promise<void> {
-    console.log(`I am ready!\n${ client.user.username } run in:`);
+    console.log(`I am ready!\n${client.user.username} run in:`);
 
-    for (const guild of client.guilds.cache) {
-      console.log(`\t${guild[1].name} with ${guild[1].memberCount} users`);
+    for (const [, guild] of client.guilds.cache) {
+      console.log(`\t${guild.name} with ${guild.memberCount} users`);
     }
 
-    const activityInfo = await client.user.setActivity({
-      name: 'name',
-      type: 'WATCHING'
-    });
+    const activityInfo = await client.user.setActivity(options.activity);
     console.log(`Set status: ${ activityInfo.status }, ${ activityInfo.activities }`);
   }
 };
