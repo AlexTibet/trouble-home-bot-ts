4e9b80f6176f6a3398a8320278e1c633075e73f1
Alexandr_Sobko <alexfreetibet@gmail.com>
2021-12-07 16:26:25 +0700

    update message event

--- src/bot/events/messageCreate.ts
+++ src/bot/events/messageCreate.ts
@@ -1,13 +1,31 @@
 import { Message } from 'discord.js';
 import { IEvent } from '../interfaces';
 import { MessageHandler } from '../handlers';
+import { bot } from '../index';
+import options from '../../config/options';
+const { prefix } = options.commands;
 
 const event: IEvent = {
   name: 'messageCreate',
-  once: true,
+  once: false,
   async execute(message: Message): Promise<void> {
     if (message.author.bot) {
       return;
+    }
+
+    const [commandName, ...args] = message.content.slice(prefix.length).split(/ +/);
+    const command = bot.commands.find((command) => command.name === commandName);
+
+    if (command) {
+      command.execute(message, args);
+    }
+
+    console.log(message.channel.id);
+    switch (message.channel.id) {
+      case '593300230263406602':
+        console.log('CHANNEL!');
+        break;
+      default: break;
     }
     const handler = new MessageHandler(message);
     await handler.execute();
