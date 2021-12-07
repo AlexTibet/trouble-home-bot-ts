8f4bd0f04354129e7f862ac3e853751f327f3674
Alexandr_Sobko <alexfreetibet@gmail.com>
2021-12-07 17:08:54 +0700

    add doc in error in warn

--- src/bot/events/error.ts
+++ src/bot/events/error.ts
@@ -1,10 +1,11 @@
 import { IEvent } from '../interfaces';
+import { doc } from '../../config/documentation';
 
 const event: IEvent = {
   name: 'error',
   once: false,
   async execute(err) {
-    console.error(`ERROR info: ${ err }`);
+    console.error(`${doc.logging.error}${ err }`);
   }
 };
 export = event;
--- src/bot/events/warn.ts
+++ src/bot/events/warn.ts
@@ -1,10 +1,11 @@
 import { IEvent } from '../interfaces';
+import { doc } from '../../config/documentation';
 
 const event: IEvent = {
   name: 'warn',
   once: false,
   async execute(warn) {
-    console.warn(`WARNING info: ${warn}`);
+    console.warn(`${doc.logging.warning}${warn}`);
   }
 };
 export = event;
